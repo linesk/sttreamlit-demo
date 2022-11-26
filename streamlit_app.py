1cfd0f41c03f92f46ccf3caedd102eddabdcab45
Ratchapoom <Ratchapoom.k@kkumail.com>
2022-11-27 00:32:52 +0700

    Day 18

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,3 +110,17 @@ st.write('Selected number from slider widget is:', number)
 st.title('Day 17 st.secrets')
 
 st.write(st.secrets['message'])
+
+st.title('Day 18 st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
