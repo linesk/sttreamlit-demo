bf9616cb7074ce0618a8d57fe28343231fedd625
Ratchapoom <Ratchapoom.k@kkumail.com>
2022-11-27 00:32:16 +0700

    Day 18

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,86 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+
+
+st.header('Day 9 st.line_chart')
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+st.header('Day 10 st.selectbox')
+
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
+
+st.header('Day 11 st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
+
+st.header('Day 12 st.checkbox')
+
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
+     
+     
+# st.header('Day 14 `streamlit_pandas_profiling`')
+
+# import pandas_profiling
+# from streamlit_pandas_profiling import st_profile_report
+
+# df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+# pr = df.profile_report()
+# st_profile_report(pr)
+
+
+st.header('Day 15 st.latex')
+
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
+
+st.title('Day 16 Customizing the theme of Streamlit apps')
+
+st.write('Contents of the `.streamlit/config.toml` file of this app')
+
+st.code("""
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
+""")
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
+
+st.title('Day 17 st.secrets')
+
+st.write(st.secrets['message'])
